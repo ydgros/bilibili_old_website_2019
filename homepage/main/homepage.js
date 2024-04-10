cf43dcc8fada8fe63d68c0fd36070de2e28ba5a9
ydgros <65645158+ydgros@users.noreply.github.com>
2024-04-10 08:19:29 +0800

    Update homepage.js

--- homepage/main/homepage.js
+++ homepage/main/homepage.js
@@ -4,7 +4,8 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.bilibili.com/
+// @match        *://www.bilibili.com/
+// @match        *://www.bilibili.com/?spm_id_from=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
 // @run-at       document-start
